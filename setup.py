eefef6be99622358a925462cddab6874cb15aaf2
Horea Christian <h.chr@mail.ru>
2014-08-15 08:28:41 +0200

    packaged for pip

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from distutils.core import setup
-setup(name="chr_helpers",
-    version="",
-    py_modules=["chr_helpers"],
+setup(
+    name="chr_helpers",
+    packages = ["chr_helpers"], # this must be the same as the name above
+    version="9999",
+    description = "Helper function library for Chymera projects",
+    author = "Horea Christian",
+    author_email = "h.chr@mail.ru",
+    url = "https://github.com/TheChymera/chr-helpers",
+    download_url = "https://github.com/TheChymera/chr-helpers",
+    heywords = ["library", "matplotlib", "pandas"],
+    py_modules = ["chr_helpers"],
+    classifiers = [],
     install_requires=[
 	"pandas"
     ]
